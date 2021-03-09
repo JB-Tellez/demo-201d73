93ead183264b600d0e9c33847fa4a3c0fd05d07e
JB-Tellez <jb.tellez@gmail.com>
2021-03-09 12:18:20 -0800

    wires up files and code demo

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// some data types
+let a = 'some text';
+let b = 4;
+let c = true;
+let d = [1,4,"some string",7,true,5,false];
+console.log(typeof(d));
+
+/*
+// conditionals
+let age = 19;
+
+if(age === 18) {
+  alert('vote');
+} else if (age === 19) {
+  alert('one year with vote')
+} else {
+  alert('other case :(')
+}
+
+switch(age) {
+  case(17):
+    alert('almost can vote');
+    break;
+  case(18):
+    alert('you just got the vote');
+    break;
+  case(19):
+    alert('one year with vote');
+    break;
+  default:
+    alert('some other case');
+}
+*/
+
+alert('mixed Case TEXT 123!?'.toUpperCase());
